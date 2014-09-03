fc99332e784cabf05d8a2f7bfa68530c02e3075a
Apprentice <apprentice@master-inf-8.local>
2014-09-03 12:23:36 -0700

    functioning Backbone QuizView

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,28 @@
-var QuizView = Backbone.View.extend({
-  el: $(".quiz"),
+
+  var QuizView = Backbone.View.extend({
+  el: $("body"),
+
+  events: {
+    'click #take-quiz': 'displayQuiz'
+  },
 
   initialize: function(){
     _.bindAll(this, 'render');
-    // this.render();
+    this.render();
   },
 
   render: function(){
     console.log(this.el)
+  },
+
+  displayQuiz: function() {
+    alert("OMGEE");
   }
+
 })
 
+
 var quizView = new QuizView();
-quizView.render();
+
+
+
